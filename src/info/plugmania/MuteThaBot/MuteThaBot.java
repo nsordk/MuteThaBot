ac16d470f1465c3e8cff73945104aecee1b5b9a9
nsordk <nicolai@nsor.dk>
2012-07-16 22:27:06 +0200

    Added Authorisation instance and Eventhandler for Player Chat

--- src/info/plugmania/MuteThaBot/MuteThaBot.java
+++ src/info/plugmania/MuteThaBot/MuteThaBot.java
@@ -1,7 +1,11 @@
 package info.plugmania.MuteThaBot;
 
+import java.util.Random;
+
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -13,23 +17,49 @@ public class MuteThaBot extends JavaPlugin {
 	}
 	
 	public void onEnable(){
-		getServer().getPluginManager().registerEvents(new LoginListener(), this);
+		getServer().getPluginManager().registerEvents(new PluginListener(), this);
 		getLogger().info("MuteThaBot is enabled.");
 	}
 	
-	public class LoginListener implements Listener {
+	public class PluginListener implements Listener {
 	    @EventHandler
 	    public void onPlayerLogin(PlayerJoinEvent event) {
-	    	event.getPlayer().sendMessage("We know you are here");
-	    	getLogger().info("Starting auth on " + event.getPlayer().getName());
+	    	Authorisation auth = new Authorisation(false,event.getPlayer());
+	    	auth.startAuth(event.getPlayer());	    
+	    }
+	    
+	    @EventHandler
+	    public void onPlayerChat(PlayerChatEvent event) {
+	    	getLogger().info("Tracked:" + event.getMessage());
 	    }
 	}
-
-	public MuteThaBot(){
-		char username;
-		char promptVal;
-		
+	
+	public MuteThaBot() {
 		
 	}
+	
+	class Authorisation {
+		boolean isAuth;
+		String name;
+		Player player;
+		char rndChar;
+			
+		Authorisation(boolean isAuth, Player player) {
+			name = player.getName();
+			rndChar = genRandChar();
+		}
+		
+		char genRandChar() {
+	    	Random r = new Random();
+	    	char c = (char)(r.nextInt(26) + 'a');
+	    	return c;
+		}
+		
+		void startAuth(Player player){
+			getLogger().info("Starting auth on " + name);
+			player.sendMessage("Please type '" + rndChar + "' now to authenticate yourself");
+		}
+	}
+	
 	
 }
